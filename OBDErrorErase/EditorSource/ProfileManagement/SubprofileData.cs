93a6985af3dc2197e4b3a7c293b1abfe73737c5a
Tom Lyden <tom.lyden.zzz@gmail.com>
2023-02-12 16:41:40 +0200

    removed unnecessary inheritance from subprofiledata

--- OBDErrorErase/EditorSource/ProfileManagement/SubprofileData.cs
+++ OBDErrorErase/EditorSource/ProfileManagement/SubprofileData.cs
@@ -1,21 +1,10 @@
 ﻿using OBDErrorErase.EditorSource.Maps;
-using System.Collections;
-using System.ComponentModel;
 
 namespace OBDErrorErase.EditorSource.ProfileManagement
 {
-    public class SubprofileData : IListSource
+    public class SubprofileData
     {
         private uint mapLength;
         private List<BaseMap> maps = new();
-
-        public bool ContainsListCollection => throw new NotImplementedException();
-
-        public IList GetList()
-        {
-            BindingList<BaseMap> list = new BindingList<BaseMap>(maps);
-
-            return list;
-        }
     }
 }
