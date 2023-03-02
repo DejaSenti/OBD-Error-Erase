db1636cf10b00ab2e9feb541ce8c7de0ab5fce3d
DejaSenti <60943224+DejaSenti@users.noreply.github.com>
2023-03-02 22:51:47 +0200

    implemented delphi processing

--- OBDErrorErase/EditorSource/Processors/DelphiErrorProcessor.cs
+++ OBDErrorErase/EditorSource/Processors/DelphiErrorProcessor.cs
@@ -12,7 +12,34 @@ namespace OBDErrorErase.EditorSource.Processors
 
         public int Process(BinaryFile file, SubprofileData subprofile, List<string> errors, List<int> mapIndices)
         {
-            throw new NotImplementedException();
+            MapDelphi? map = subprofile.Maps.FirstOrDefault() as MapDelphi;
+
+            if (map == null)
+            {
+                return 0;
+            }
+
+            // write new value
+            int start = file.FindValue(map.SearchWord, 0, file.Length);
+
+            int totalErased = 0;
+            // for each error
+            foreach (var error in errors)
+            {
+                // find location, round down according to column number
+                byte[] byteError = Convert.FromHexString(error);
+                var location = file.FindValue(byteError, start, start + subprofile.MapLength);
+                
+                if (location == -1)
+                    continue;
+
+                location -= map.ErrorColumn - 1;
+
+                file.WriteValue(location, map.NewValue.ToArray());
+                ++totalErased;
+            }
+
+            return totalErased;
         }
     }
 }
